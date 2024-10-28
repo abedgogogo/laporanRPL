dce01542fec9630343d2a841801c9f49db83b8fe
indra9089 <indraprasetyo643@gmail.com>
2024-10-28 08:59:16 +0700

    first commit

---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class Baseurl {
+  static String tambah = "http://192.168.137.1/BIODATA/create.php";
+  static String data = "http://192.168.137.1/BIODATA/list.php";
+  static String lihat = "http://192.168.137.1/BIODATA/details.php";
+  static String edit = "http://192.168.137.1/BIODATA/update.php";
+  static String hapus = "http://192.168.137.1/BIODATA/delete.php";
+}
